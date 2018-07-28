55e3bd757e5425ee6eac0dfd989b925aeee92e03
MichalMarchwinski <michal.marchwinski@gmail.com>
2018-07-28 15:46:45 +0200

    Excersice to implement my own List

---
+++ src/main/java/com/sdacademy/listy/CustomListImplementation.java
@@ -0,0 +1,75 @@
+package com.sdacademy.listy;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class CustomListImplementation<E> implements Iterable<E>{
+
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public CustomListImplementation(){
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e) {
+        if(size==elements.length) {
+            resizeArray();
+        }
+        elements[size++] = e;
+    }
+
+    public void resizeArray(){
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public E get(int i) {
+        if(i>=size || i<0) {
+            throw new IndexOutOfBoundsException("Zły element");
+        }
+        return (E) elements[i];
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return ArrayListIterator;
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return null;
+    }
+
+    private class ArrayListIterator implements java.util.Iterator<E>{
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public E next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+
+        }
+    }
+}
---
+++ src/main/java/com/sdacademy/listy/Main.java
@@ -0,0 +1,30 @@
+package com.sdacademy.listy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CustomListImplementation<Integer> list = new CustomListImplementation<Integer>();
+
+        list.add(2);
+        list.add(2);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+
+        System.out.println(list.get(4));
+
+        for (CustomListImplementation list1: list)
+
+
+    }
+}
